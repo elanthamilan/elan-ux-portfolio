426a209c015038405b95ac817775aad0222e11fa
elanthamilan <elanthamilan12@gmail.com>
2025-05-08 12:38:55 +0530

    v5

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/elan-ux-portfolio/',
   server: {
     host: "::",
     port: 8080,
